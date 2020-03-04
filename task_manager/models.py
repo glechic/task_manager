06f35164143cb117773d7bbc6f387a1c2ec746cf
glech <glech@glechpc>
2020-03-04 18:37:56 +0300

    updated task model by adding status field

--- task_manager/models.py
+++ task_manager/models.py
@@ -10,9 +10,18 @@ class Project(models.Model):
         return self.title
 
 class Task(models.Model):
+
+    TODO = 'T'
+    DONE = 'D'
+    STATUS = (
+        (TODO, 'TODO'),
+        (DONE, 'DONE'),
+    )
+
     title = models.CharField(max_length=32)
     description = models.CharField(max_length=128)
     due_date = models.DateField()
+    status = models.CharField(max_length=1, choices=STATUS, default=TODO, null=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
